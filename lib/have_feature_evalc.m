14dd97ea792bbe3af8414bd11f5533edb23729b2
Ray Zimmerman <rz10@cornell.edu>
2020-09-21 12:34:24 -0500

    Add notes in have_feature_evalc() about Octave.

--- lib/have_feature_evalc.m
+++ lib/have_feature_evalc.m
@@ -11,14 +11,23 @@ function [TorF, vstr, rdate] = have_feature_evalc()
 %   Covered by the 3-clause BSD License (see LICENSE file for details).
 %   See https://github.com/MATPOWER/mp-opt-model for more info.
 
-%% ignore evalc in Octave (I don't remember why)
-v = have_feature('matlab', 'all');
-if v.av
-    TorF = 1;
-    vstr = v.vstr;
+TorF = exist('evalc') ~= 0;
+if TorF
+    if have_feature('matlab')
+        v = have_feature('matlab', 'all');
+    else
+        v = have_feature('octave', 'all');
+        TorF = 0;   %% In Octave, evalc was introduced in 4.2.x, but as of 6.x
+                    %% we still choose to ignore evalc in Octave since it does
+                    %% not capture the output of all functions (e.g. .mex/.oct
+                    %% functions such as ipopt, glpk), which is one of the
+                    %% main reasons we'd like to use it. Not only does it not
+                    %% capture the output, it also does not prevent it from
+                    %% going to the console.
+    end
+    vstr  = v.vstr;
     rdate = v.date;
 else
-    TorF = 0;
     vstr = '';
     rdate = '';
 end
