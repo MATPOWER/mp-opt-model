1be636aa531b879edc522743571594caa7816c0d
Ray Zimmerman <rz10@cornell.edu>
2025-05-21 09:07:14 -0600

    Duplicate t_opt_model as t_mp_opt_model, and t_om_solve_<type> as t_mm_solve_<type>.
    Convert duplicated files from using opt_model to mp_opt_model.
    Update all duplicated code for new mp.set_manager syntax.
    Add new tests to test_mp_opt_model.

---
+++ lib/t/t_mm_solve_nleqs.m
@@ -0,0 +1,263 @@
+function t_mm_solve_nleqs(quiet)
+% t_mm_solve_nleqs - Tests of NLEQ solvers via mp.opt_model.solve.
+
+%   MP-Opt-Model
+%   Copyright (c) 2010-2025, Power Systems Engineering Research Center (PSERC)
+%   by Ray Zimmerman, PSERC Cornell
+%
+%   This file is part of MP-Opt-Model.
+%   Covered by the 3-clause BSD License (see LICENSE file for details).
+%   See https://github.com/MATPOWER/mp-opt-model for more info.
+
+if nargin < 1
+    quiet = 0;
+end
+
+core_sp_newton = struct( ...
+    'alg',              'NEWTON', ...
+    'name',             'Newton''s', ...
+    'default_max_it',   10, ...
+    'need_jac',         1, ...
+    'update_fcn',       @(x, f, J)newton_update_fcn(x, f, J, '')  );
+core_sp_gs = struct( ...
+    'alg',              'GS', ...
+    'name',             'Gauss-Seidel', ...
+    'default_max_it',   1000, ...
+    'need_jac',         0, ...
+    'update_fcn',       @(x, f)x_update_fcn2(x, f)  );
+
+if have_feature('matlab')
+    %%  alg         name        check       opts
+    cfg = {
+        {'DEFAULT', 'default',  []          []  },
+        {'NEWTON',  'Newton',   [],         []  },
+        {'FD',      'fast-decoupled Newton',[],[]  },
+        {'FSOLVE',  'fsolve-1', 'fsolve',   []  },
+        {'FSOLVE',  'fsolve-2', 'fsolve',   struct('Algorithm', 'trust-region-dogleg')  },
+        {'FSOLVE',  'fsolve-3', 'fsolve',   struct('Algorithm', 'trust-region-reflective')  },
+        {'FSOLVE',  'fsolve-4', 'fsolve',   struct('Algorithm', 'levenberg-marquardt', 'TolX', 1e-11) },
+        {'GS',      'Gauss-Seidel',[],      []  },
+        {'CORE-N',  'Newton-CORE',   [],    core_sp_newton  },
+        {'CORE-GS', 'Gauss-Seidel-CORE',[], core_sp_gs  },
+    };
+    if have_feature('matlab', 'vnum') <= 7.010
+        cfg([6]) = [];  %% MATLAB 7.10 does not work w/ fsolve alg 3
+    end
+else    %% octave
+    %%  alg         name        check       opts
+    cfg = {
+        {'DEFAULT', 'default',  []          []  },
+        {'NEWTON',  'Newton',   [],         []  },
+        {'FD',      'fast-decoupled Newton',[],[]  },
+        {'FSOLVE',  'fsolve', 'fsolve',     struct('TolX', 1e-11)  },
+        {'GS',      'Gauss-Seidel',[],      []  },
+        {'CORE-N',  'Newton-CORE',   [],    core_sp_newton  },
+        {'CORE-GS', 'Gauss-Seidel-CORE',[], core_sp_gs  },
+    };
+end
+
+n = 18;
+diff_tool = 'bbdiff';
+show_diff_on_fail = false;
+
+reps = {};
+
+t_begin(16+n*length(cfg), quiet);
+
+for k = 1:length(cfg)
+    alg   = cfg{k}{1};
+    name  = cfg{k}{2};
+    check = cfg{k}{3};
+    opts  = cfg{k}{4};
+    if ~isempty(check) && ~have_feature(check)
+        t_skip(n, sprintf('%s not installed', name));
+    else
+        opt = struct('verbose', 0, 'alg', alg, 'tol', 1e-11);
+        switch alg
+            case {'DEFAULT', 'NEWTON'}
+            case {'FD'}
+                opt.fd_opt.jac_approx_fcn = @jac_approx_fcn2;
+            case 'FSOLVE'
+                opt.fsolve_opt = opts;
+            case 'GS'
+                opt.gs_opt.x_update_fcn = @(x, f)x_update_fcn2(x, f);
+            case {'CORE-N', 'CORE-GS'}
+                opt.core_sp = opts;
+                opt.alg = 'CORE';
+        end
+
+        switch alg
+            case {'DEFAULT', 'NEWTON', 'FSOLVE', 'CORE-N'}
+                t = sprintf('%s - 2-d function : ', name);
+                x0 = [-1;0];
+                om = mp.opt_model;
+                om.var.add('x', 2, x0);
+                om.nle.add(om.var, 'f', 2, @f1, []);
+                [x, f, e, out, jac] = om.solve(opt);
+                t_is(e, 1, 12, [t 'success']);
+                t_is(x, [-3; 4], 8, [t 'x']);
+                t_is(f, 0, 10, [t 'f']);
+                switch alg
+                    case {'DEFAULT', 'CORE-N'}
+                        out_alg = 'NEWTON';
+                    otherwise
+                        out_alg = alg;
+                end
+                t_str_match(out.alg, out_alg, [t 'out.alg']);
+                eJ = [1 1; 6 1];
+                t_is(jac, eJ, 5.8, [t 'jac']);
+
+                t = sprintf('%s - 2-d function (max_it) : ', name);
+                opt.max_it = 3;
+                [x, f, e, out] = om.solve(opt);
+                t_is(e, 0, 12, [t 'no success']);
+                t_ok(out.iterations == 3 || out.iterations == 4, [t 'iterations']);
+                opt = rmfield(opt, 'max_it');
+
+                t = sprintf('%s - 5-d lin/nonlin function : ', name);
+                x0_1 = [-1;0];
+                x0_2 = [0;0;0];
+                A2 = sparse([2 -1 0; -3 1 -2; 0 5 -4]);
+                b2 = [-5; 1; -7];
+                x2 = [-2; 1; 3];
+                om = mp.opt_model;
+                om.var.add('x1', 2, x0_1);
+                om.var.add('x2', 3, x0_2);
+                om.nle.add(om.var, 'f', 2, @f1, [], {'x1'});
+                om.lin.add(om.var, 'Ax_b', A2, b2, b2, {'x2'});
+                [x, f, e, out, jac] = om.solve(opt);
+                t_is(e, 1, 12, [t 'success']);
+                t_is(x, [-3; 4; -2; 1; 3], 8, [t 'x']);
+                t_is(f, 0, 10, [t 'f']);
+                t_str_match(out.alg, out_alg, [t 'out.alg']);
+                eJ = [[1 1; 6 1] zeros(2, 3);
+                      zeros(3, 2) A2 ];
+                t_is(jac, eJ, 5.8, [t 'jac']);
+            otherwise
+                t_skip(12, sprintf('not implemented for solver ''%s''', alg));
+        end
+
+        t = sprintf('%s - 2-d function2 (struct) : ', name);
+        x0 = [1;2];
+        om = mp.opt_model;
+        om.var.add('x', 2, x0);
+        om.nle.add(om.var, 'f', 2, @f2, []);
+        [x, f, e] = om.solve(opt);
+        t_is(e, 1, 12, [t 'success']);
+        t_is(x, [2; 3], 8, [t 'x']);
+        t_is(f, 0, 10, [t 'f']);
+        t_ok(~om.has_parsed_soln(), [t 'has_parsed_soln() is false']);
+
+        opt.max_it = 3;
+        t = sprintf('%s - 2-d function2 (max_it) : ', name);
+        [x, f, e, out] = om.solve(opt);
+        t_is(e, 0, 12, [t 'no success']);
+        t_ok(out.iterations == 3 || out.iterations == 4, [t 'iterations']);
+        opt = rmfield(opt, 'max_it');
+    end
+end
+
+t = 'om.soln.';
+opt.alg = 'DEFAULT';
+x0_1 = [-1;0];
+x0_2 = [0;0;0];
+A2 = sparse([2 -1 0; -3 1 -2; 0 5 -4]);
+b2 = [-5; 1; -7];
+x2 = [-2; 1; 3];
+om = mp.opt_model;
+om.var.add('x1', 2, x0_1);
+om.var.add('x2', 3, x0_2);
+om.nle.add(om.var, 'f', 2, @f1, [], {'x1'});
+om.lin.add(om.var, 'Ax_b', A2, b2, b2, {'x2'});
+opt.parse_soln = 1;
+[x, f, e, out, jac] = om.solve(opt);
+t_is(om.soln.x, x, 14, [t 'x']);
+t_is(om.soln.f, f, 14, [t 'f']);
+t_is(om.soln.eflag, e, 14, [t 'eflag']);
+t_str_match(om.soln.output.alg, out.alg, [t 'output.alg']);
+t_is(om.soln.jac, jac, 14, [t 'jac']);
+
+t = 'om.var.get_soln(om.soln, ''x1'') : ';
+t_is(om.var.get_soln(om.soln, 'x1'), x(1:2), 14, [t 'x1']);
+
+t = 'om.var.get_soln(om.soln, ''x'', ''x2'') : ';
+t_is(om.var.get_soln(om.soln, 'x', 'x2'), x(3:5), 14, [t 'x2']);
+
+t = 'om.lin.get_soln(om.var, om.soln, ''g'', ''Ax_b'') : ';
+g = om.lin.get_soln(om.var, om.soln, 'g', 'Ax_b');
+t_is(g{1}, f(3:5), 14, [t 'A * x - u']);
+t_is(g{2}, f(3:5), 14, [t 'l - A * x']);
+
+t = 'om.nle.get_soln(om.var, om.soln, true, ''f'') : ';
+g = om.nle.get_soln(om.var, om.soln, true, 'f');
+t_is(g, f(1:2), 14, [t 'f']);
+
+t = 'om.nle.get_soln(om.var, om.soln, true, {''g'', ''dg''}, ''f'') : ';
+[g, dg] = om.nle.get_soln(om.var, om.soln, true, {'g', 'dg'}, 'f');
+t_is(g, f(1:2), 14, [t 'f']);
+t_is(dg, jac(1:2, 1:2), 14, [t 'jac']);
+
+t = 'parse_soln : ';
+t_ok(om.has_parsed_soln(), [t 'has_parsed_soln() is true']);
+t_is(om.var.soln.val.x1, om.var.get_soln(om.soln, 'x1'), 14, [t 'var.val.x1']);
+t_is(om.var.soln.val.x2, om.var.get_soln(om.soln, 'x2'), 14, [t 'var.val.x2']);
+
+t = 'disp_soln';
+rn = fix(1e9*rand);
+[pathstr, name, ext] = fileparts(which('t_opt_model'));
+fname = 't_om_solve_nleqs_display_soln';
+fname_e = fullfile(pathstr, 'display_soln', sprintf('%s.txt', fname));
+fname_g = sprintf('%s_%d.txt', fname, rn);
+[fd, msg] = fopen(fname_g, 'wt');   %% open solution file
+if fd == -1
+    error('t_om_solve_nleqs: could not create %d : %s', fname, msg);
+end
+om.display_soln(fd);    %% write out solution
+fclose(fd);
+if ~t_file_match(fname_g, fname_e, t, reps, 1);
+    fprintf('  compare these 2 files:\n    %s\n    %s\n', fname_g, fname_e);
+    if show_diff_on_fail
+        cmd = sprintf('%s %s %s', diff_tool, fname_g, fname_e);
+        [status, result] = system(cmd);
+        keyboard
+    end
+end
+
+t_end;
+
+
+%% 2-d problem with 2 solutions
+%% from https://www.chilimath.com/lessons/advanced-algebra/systems-non-linear-equations/
+function [f, J] = f1(x)
+if iscell(x)    %% in case it was part of a varset
+    x = x{1};
+end
+f = [  x(1)   + x(2) - 1;
+      -x(1)^2 + x(2) + 5    ];
+if nargout > 1
+    J = sparse([1 1; -2*x(1) 1]);
+end
+
+%% another 2-d problem
+%% from Christi Patton Luks, https://www.youtube.com/watch?v=pJG4yhtgerg
+function [f, J] = f2(x)
+f = [  x(1)^2 +   x(1)*x(2)   - 10;
+       x(2)   + 3*x(1)*x(2)^2 - 57  ];
+if nargout > 1
+    J = sparse([    2*x(1)+x(2)    x(1);
+                    3*x(2)^2       6*x(1)*x(2)+1    ]);
+end
+
+function JJ = jac_approx_fcn2()
+%% for use with fast-decoupled Newton's method
+J = [7 2; 27 37];
+JJ = {J(1,1), J(2,2)};
+
+function x = x_update_fcn2(x, f)
+%% for use with Gauss-Seidel method
+x(1) = sqrt(10 - x(1)*x(2));
+x(2) = sqrt((57-x(2))/3/x(1));
+
+function x = newton_update_fcn(x, f, J, lin_solver)
+dx = mplinsolve(J, -f, lin_solver);     %% compute update step
+x = x + dx;                             %% update x
---
+++ lib/t/t_mm_solve_qps.m
@@ -0,0 +1,317 @@
+function t_mm_solve_qps(quiet)
+% t_mm_solve_qps - Tests of QP solvers via mp.opt_model.solve.
+
+%   MP-Opt-Model
+%   Copyright (c) 2010-2025, Power Systems Engineering Research Center (PSERC)
+%   by Ray Zimmerman, PSERC Cornell
+%   and Wilson Gonzalez Vanegas, Universidad Nacional de Colombia Sede Manizales
+%
+%   This file is part of MP-Opt-Model.
+%   Covered by the 3-clause BSD License (see LICENSE file for details).
+%   See https://github.com/MATPOWER/mp-opt-model for more info.
+
+if nargin < 1
+    quiet = 0;
+end
+
+algs = {'DEFAULT', 'BPMPD', 'MIPS', 250, 'IPOPT', 'OT', 'CPLEX', 'MOSEK', 'GUROBI', 'CLP', 'GLPK', 'OSQP', 'KNITRO'};
+names = {'DEFAULT', 'BPMPD_MEX', 'MIPS', 'sc-MIPS', 'IPOPT', 'linprog/quadprog', 'CPLEX', 'MOSEK', 'Gurobi', 'CLP', 'glpk', 'OSQP', 'Knitro'};
+check = {[], 'bpmpd', [], [], 'ipopt', 'quadprog', 'cplex', 'mosek', 'gurobi', 'clp', 'glpk', 'osqp', 'knitro'};
+does_qp = [1 1 1 1 1 1 1 1 1 1 0 1 1];
+
+n = 30;
+nqp = 21;
+diff_tool = 'bbdiff';
+show_diff_on_fail = false;
+
+reps = {};
+
+t_begin(29+n*length(algs), quiet);
+
+diff_alg_warn_id = 'optim:linprog:WillRunDiffAlg';
+if have_feature('quadprog') && have_feature('quadprog', 'vnum') == 7.005
+    s1 = warning('query', diff_alg_warn_id);
+    warning('off', diff_alg_warn_id);
+end
+
+for k = 1:length(algs)
+    if ~isempty(check{k}) && ~have_feature(check{k})
+        t_skip(n, sprintf('%s not installed', names{k}));
+    else
+        opt = struct('verbose', 0, 'alg', algs{k});
+        mpopt = struct( ...
+            'verbose', 0, ...
+            'opf', struct( ...
+                'violation', 1e-8 ), ...
+            'cplex', struct( ...
+                'lpmethod', 0, ...
+                'qpmethod', 0, ...
+                'opt', 0 ), ...
+            'mosek', struct( ...
+                'lp_alg', 0, ...
+                'max_it', 0, ...
+                'gap_tol', 0, ...
+                'max_time', 0, ...
+                'num_threads', 0, ...
+                'opt', 0 ), ...
+            'knitro', struct( ...
+                 'tol_x', 0, ...
+                 'tol_f', 0, ...
+                 'maxit', 0, ...
+                 'opt', 0) ...
+        );
+        opt.mips_opt.comptol = 1e-8;
+%         if have_feature('linprog')
+%             opt.linprog_opt.Algorithm = 'interior-point';
+%             opt.linprog_opt.Algorithm = 'active-set';
+%             opt.linprog_opt.Algorithm = 'simplex';
+%             opt.linprog_opt.Algorithm = 'dual-simplex';
+%         end
+        if have_feature('cplex')
+            % alg = 0;        %% default uses barrier method with NaN bug in lower lim multipliers
+            alg = 2;        %% use dual simplex
+            mpopt.cplex.lpmethod = alg;
+            mpopt.cplex.qpmethod = min([4 alg]);
+            opt.cplex_opt = cplex_options([], mpopt);
+        end
+        if have_feature('mosek')
+%             sc = mosek_symbcon;
+%             alg = sc.MSK_OPTIMIZER_DUAL_SIMPLEX;    %% use dual simplex
+%             alg = sc.MSK_OPTIMIZER_INTPNT;          %% use interior point
+%             mpopt.mosek.lp_alg = alg;
+            mpopt.mosek.gap_tol = 1e-10;
+%             mpopt.mosek.opts.MSK_DPAR_INTPNT_TOL_PFEAS = 1e-10;
+%             mpopt.mosek.opts.MSK_DPAR_INTPNT_TOL_DFEAS = 1e-10;
+%             mpopt.mosek.opts.MSK_DPAR_INTPNT_TOL_INFEAS = 1e-10;
+%             mpopt.mosek.opts.MSK_DPAR_INTPNT_TOL_REL_GAP = 1e-10;
+            vnum = have_feature('mosek', 'vnum');
+            if vnum >= 8
+%                 mpopt.mosek.opts.MSK_DPAR_INTPNT_QO_TOL_PFEAS = 1e-10;
+%                 mpopt.mosek.opts.MSK_DPAR_INTPNT_QO_TOL_DFEAS = 1e-10;
+%                 mpopt.mosek.opts.MSK_DPAR_INTPNT_QO_TOL_INFEAS = 1e-10;
+%                 mpopt.mosek.opts.MSK_DPAR_INTPNT_QO_TOL_MU_RED = 1e-10;
+                mpopt.mosek.opts.MSK_DPAR_INTPNT_QO_TOL_REL_GAP = 1e-10;
+            end
+            opt.mosek_opt = mosek_options([], mpopt);
+        end
+        if have_feature('osqp')
+            opt.osqp_opt.polish = 1;
+%             opt.osqp_opt.alpha = 1;
+%             opt.osqp_opt.eps_abs = 1e-8;
+%             opt.osqp_opt.eps_rel = 1e-10;
+%             opt.osqp_opt.eps_prim_inf = 1e-8;
+%             opt.osqp_opt.eps_dual_inf = 1e-8;
+        end
+        if have_feature('knitro')
+            opt.knitro_opt = artelys_knitro_options([],  mpopt);
+        end
+
+        t = sprintf('%s - 3-d LP : ', names{k});
+        %% based on example from 'doc linprog'
+        c = [-5; -4; -6];
+        A = [1 -1  1;
+             -3  -2  -4;
+             3  2  0];
+        l = [-Inf; -42; -Inf];
+        u = [20; Inf; 30];
+        xmin = [0; 0; 0];
+        x0 = [];
+        om = mp.opt_model;
+        om.var.add('x', 3, x0, xmin);
+        om.qdc.add(om.var, 'c', [], c);
+        om.lin.add(om.var, 'Ax', A, l, u);
+        [x, f, s, out, lam] = om.solve(opt);
+        t_is(s, 1, 12, [t 'success']);
+        t_is(x, [0; 15; 3], 6, [t 'x']);
+        t_is(f, -78, 6, [t 'f']);
+        t_is(lam.mu_l, [0;1.5;0], 9, [t 'lam.mu_l']);
+        t_is(lam.mu_u, [0;0;0.5], 9, [t 'lam.mu_u']);
+        if strcmp(algs{k}, 'CLP') && ~have_feature('opti_clp')
+            t_skip(2, [t 'lam.lower/upper : MEXCLP does not return multipliers on var bounds']);
+        else
+            t_is(lam.lower, [1;0;0], 9, [t 'lam.lower']);
+            t_is(lam.upper, zeros(size(x)), 9, [t 'lam.upper']);
+        end
+        t_ok(~om.has_parsed_soln(), [t 'has_parsed_soln() is false']);
+
+        if does_qp(k)
+            t = sprintf('%s - unconstrained 3-d quadratic : ', names{k});
+            %% from http://www.akiti.ca/QuadProgEx0Constr.html
+            H = [5 -2 -1; -2 4 3; -1 3 5];
+            c = [2; -35; -47];
+            x0 = [0; 0; 0];
+            om = mp.opt_model;
+            om.var.add('x', 3, x0);
+            om.qdc.add(om.var, 'c', H, c);
+            [x, f, s, out, lam] = om.solve(opt);
+            t_is(s, 1, 12, [t 'success']);
+            t_is(x, [3; 5; 7], 7, [t 'x']);
+            t_is(f, -249, 11, [t 'f']);
+            t_ok(isempty(lam.mu_l), [t 'lam.mu_l']);
+            t_ok(isempty(lam.mu_u), [t 'lam.mu_u']);
+            t_is(lam.lower, zeros(size(x)), 13, [t 'lam.lower']);
+            t_is(lam.upper, zeros(size(x)), 13, [t 'lam.upper']);
+
+            t = sprintf('%s - constrained 2-d QP : ', names{k});
+            %% example from 'doc quadprog'
+            H = [   1   -1;
+                    -1  2   ];
+            c = [-2; -6];
+            A = [   1   1;
+                    -1  2;
+                    2   1   ];
+            l = [];
+            u = [2; 2; 3];
+            xmin = [0; 0];
+            x0 = [];
+            om = mp.opt_model;
+            om.var.add('x', 2, x0, xmin);
+            om.qdc.add(om.var, 'c', H, c);
+            om.lin.add(om.var, 'Ax', A, l, u);
+            [x, f, s, out, lam] = om.solve(opt);
+            t_is(s, 1, 12, [t 'success']);
+            t_is(x, [2; 4]/3, 7, [t 'x']);
+            t_is(f, -74/9, 6, [t 'f']);
+            t_is(lam.mu_l, [0;0;0], 13, [t 'lam.mu_l']);
+            t_is(lam.mu_u, [28;4;0]/9, 4, [t 'lam.mu_u']);
+            if strcmp(algs{k}, 'CLP') && ~have_feature('opti_clp')
+                t_skip(2, [t 'lam.lower/upper : MEXCLP does not return multipliers on var bounds']);
+            else
+                t_is(lam.lower, zeros(size(x)), 7, [t 'lam.lower']);
+                t_is(lam.upper, zeros(size(x)), 13, [t 'lam.upper']);
+            end
+
+            t = sprintf('%s - constrained 4-d QP : ', names{k});
+            %% from https://v8doc.sas.com/sashtml/iml/chap8/sect12.htm
+            H = [   1003.1  4.3     6.3     5.9;
+                    4.3     2.2     2.1     3.9;
+                    6.3     2.1     3.5     4.8;
+                    5.9     3.9     4.8     10  ];
+            c = zeros(4,1);
+            A = [   1       1       1       1;
+                    0.17    0.11    0.10    0.18    ];
+            l = [1; 0.10];
+            u = [1; Inf];
+            xmin = zeros(4,1);
+            x0 = [1; 0; 0; 1];
+            om = mp.opt_model;
+            om.var.add('x', 4, x0, xmin);
+            om.qdc.add(om.var, 'c', H, c);
+            om.lin.add(om.var, 'Ax', A, l, u);
+            [x, f, s, out, lam] = om.solve(opt);
+            t_is(s, 1, 12, [t 'success']);
+            t_is(x, [0; 2.8; 0.2; 0]/3, 5, [t 'x']);
+            t_is(f, 3.29/3, 6, [t 'f']);
+            t_is(lam.mu_l, [6.58;0]/3, 6, [t 'lam.mu_l']);
+            t_is(lam.mu_u, [0;0], 13, [t 'lam.mu_u']);
+            if strcmp(algs{k}, 'CLP') && ~have_feature('opti_clp')
+                t_skip(2, [t 'lam.lower/upper : MEXCLP does not return multipliers on var bounds']);
+            else
+                t_is(lam.lower, [2.24;0;0;1.7667], 4, [t 'lam.lower']);
+                t_is(lam.upper, zeros(size(x)), 13, [t 'lam.upper']);
+            end
+        else
+            t_skip(nqp, sprintf('%s does not handle QP problems', names{k}));
+        end
+
+        t = sprintf('%s - infeasible LP : ', names{k});
+        p = struct('A', sparse([1 1]), 'c', [1;1], 'u', -1, 'xmin', [0;0], 'opt', opt);
+        [x, f, s, out, lam] = qps_master(p);
+        t_ok(s <= 0, [t 'no success']);
+    end
+end
+
+t = 'om.soln.';
+opt.alg = 'MIPS';
+%% from https://v8doc.sas.com/sashtml/iml/chap8/sect12.htm
+H = [   1003.1  4.3     6.3     5.9;
+        4.3     2.2     2.1     3.9;
+        6.3     2.1     3.5     4.8;
+        5.9     3.9     4.8     10  ];
+c = zeros(4,1);
+A = [   1       1       1       1;
+        0.17    0.11    0.10    0.18    ];
+l = [1; 0.10];
+u = [1; Inf];
+xmin = zeros(4,1);
+x0 = [1; 0; 0; 1];
+om = mp.opt_model;
+om.var.add('x', 4, x0, xmin);
+om.qdc.add(om.var, 'c', H, c);
+om.lin.add(om.var, 'Ax', A, l, u);
+opt.parse_soln = 1;
+[x, f, s, out, lam] = om.solve(opt);
+t_is(om.soln.x, x, 14, [t 'x']);
+t_is(om.soln.f, f, 14, [t 'f']);
+t_is(om.soln.eflag, s, 14, [t 'eflag']);
+t_str_match(om.soln.output.alg, out.alg, [t 'output.alg']);
+t_is(om.soln.lambda.lower, lam.lower, 14, [t 'om.soln.lambda.lower']);
+t_is(om.soln.lambda.upper, lam.upper, 14, [t 'om.soln.lambda.upper']);
+t_is(om.soln.lambda.mu_l, lam.mu_l, 14, [t 'om.soln.lambda.mu_l']);
+t_is(om.soln.lambda.mu_u, lam.mu_u, 14, [t 'om.soln.lambda.mu_u']);
+
+t = 'om.var.get_soln(om.soln, ''x'') : ';
+[x1, mu_l, mu_u] = om.var.get_soln(om.soln, 'x');
+t_is(x1, x, 14, [t 'x']);
+t_is(mu_l, lam.lower, 14, [t 'mu_l']);
+t_is(mu_u, lam.upper, 14, [t 'mu_u']);
+
+t = 'om.var.get_soln(om.soln, ''mu_l'', ''x'') : ';
+t_is(om.var.get_soln(om.soln, 'mu_l', 'x'), lam.lower, 14, [t 'mu_l']);
+
+t = 'om.lin.get_soln(om.var, om.soln, ''Ax'') : ';
+[g, mu_l] = om.lin.get_soln(om.var, om.soln, 'Ax');
+t_is(g{1}, A*x-u, 14, [t 'A * x - u']);
+t_is(g{2}, l-A*x, 14, [t 'l - A * x']);
+t_is(mu_l, lam.mu_l, 14, [t 'mu_l']);
+
+t = 'om.lin.get_soln(om.var, om.soln, {''mu_u'', ''mu_l'', ''Ax_u''}, ''Ax'') : ';
+[mu_u, mu_l, g] = om.lin.get_soln(om.var, om.soln, {'mu_u', 'mu_l', 'Ax_u'}, 'Ax');
+t_is(g, A*x-u, 14, [t 'A * x - u']);
+t_is(mu_l, lam.mu_l, 14, [t 'mu_l']);
+t_is(mu_u, lam.mu_u, 14, [t 'mu_u']);
+
+t = 'om.qdc.get_soln(om.var, om.soln, ''c'') : ';
+[f1, df, d2f] = om.qdc.get_soln(om.var, om.soln, 'c');
+t_is(f1, f, 14, [t 'f']);
+t_is(df, H*x+c, 14, [t 'df']);
+t_is(d2f, H, 14, [t 'd2f']);
+
+t = 'om.qdc.get_soln(om.var, om.soln, ''df'', ''c'') : ';
+df = om.qdc.get_soln(om.var, om.soln, 'df', 'c');
+t_is(df, H*x+c, 14, [t 'df']);
+
+t = 'parse_soln : ';
+t_ok(om.has_parsed_soln(), [t 'has_parsed_soln() is true']);
+t_is(om.var.soln.val.x, om.var.get_soln(om.soln, 'x'), 14, [t 'var.val.x']);
+t_is(om.var.soln.mu_l.x, om.var.get_soln(om.soln, 'mu_l', 'x'), 14, [t 'var.mu_l.x']);
+t_is(om.var.soln.mu_u.x, om.var.get_soln(om.soln, 'mu_u', 'x'), 14, [t 'var.mu_u.x']);
+t_is(om.lin.soln.mu_l.Ax, om.lin.get_soln(om.var, om.soln, 'mu_l', 'Ax'), 14, [t 'lin.mu_l.Ax']);
+t_is(om.lin.soln.mu_u.Ax, om.lin.get_soln(om.var, om.soln, 'mu_u', 'Ax'), 14, [t 'lin.mu_u.Ax']);
+
+t = 'disp_soln';
+rn = fix(1e9*rand);
+[pathstr, name, ext] = fileparts(which('t_opt_model'));
+fname = 't_om_solve_qps_display_soln';
+fname_e = fullfile(pathstr, 'display_soln', sprintf('%s.txt', fname));
+fname_g = sprintf('%s_%d.txt', fname, rn);
+[fd, msg] = fopen(fname_g, 'wt');   %% open solution file
+if fd == -1
+    error('t_om_solve_qps: could not create %d : %s', fname, msg);
+end
+om.display_soln(fd);    %% write out solution
+fclose(fd);
+if ~t_file_match(fname_g, fname_e, t, reps, 1);
+    fprintf('  compare these 2 files:\n    %s\n    %s\n', fname_g, fname_e);
+    if show_diff_on_fail
+        cmd = sprintf('%s %s %s', diff_tool, fname_g, fname_e);
+        [status, result] = system(cmd);
+        keyboard
+    end
+end
+
+if have_feature('quadprog') && have_feature('quadprog', 'vnum') == 7.005
+    warning(s1.state, diff_alg_warn_id);
+end
+
+t_end;
