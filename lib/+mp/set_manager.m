MATPOWER/mp-opt-model

commit ef720972b26143496f6cde0d092bcf8878e9aea4

For mp.set_manager subclasses, don't overwrite an existing data property in constructor.
Prepares to allow for mp.set_manager base class to handle construction from a struct.

lib/+mp/set_manager.m
@@ -147,7 +147,7 @@ classdef set_manager < handle
         order = struct('name', [], 'idx', []);
 
         % *(struct)* additional set-type-specific data for each block
-        data = struct();
+        data
     end     %% properties
 
     methods
@@ -156,6 +156,9 @@ classdef set_manager < handle
             % ::
             %
             %   sm = mp.set_manager(label)
+            %
+            % Input:
+            %   label (char array) : label used in display for this set type
 
             obj.label = label;
         end

lib/+mp/sm_lin_constraint.m
@@ -52,12 +52,14 @@ classdef sm_lin_constraint < mp.set_manager_opt_model
 
             es = struct();  %% empty struct
             obj@mp.set_manager_opt_model(varargin{:});
-            obj.data = struct( ...
-                'A', es, ...
-                'l', es, ...
-                'u', es, ...
-                'tr', es, ...
-                'vs', es );
+            if isempty(obj.data)
+                obj.data = struct( ...
+                    'A', es, ...
+                    'l', es, ...
+                    'u', es, ...
+                    'tr', es, ...
+                    'vs', es );
+            end
         end
 
         function obj = add(obj, var, name, idx, varargin)

lib/+mp/sm_nln_constraint.m
@@ -47,11 +47,13 @@ classdef sm_nln_constraint < mp.set_manager_opt_model
             %   nln = mp.sm_nln_constraint(label)
 
             obj@mp.set_manager_opt_model(varargin{:});
-            obj.data = struct( ...
-                'fcn', [], ...
-                'hess', [], ...
-                'include', [], ...
-                'vs', struct() );
+            if isempty(obj.data)
+                obj.data = struct( ...
+                    'fcn', [], ...
+                    'hess', [], ...
+                    'include', [], ...
+                    'vs', struct() );
+            end
         end
 
         function obj = add(obj, var, name, idx, varargin)

lib/+mp/sm_nln_cost.m
@@ -40,9 +40,11 @@ classdef sm_nln_cost < mp.set_manager_opt_model
 
             es = struct();  %% empty struct
             obj@mp.set_manager_opt_model(varargin{:});
-            obj.data = struct( ...
-                'fcn', es, ...
-                'vs', es );
+            if isempty(obj.data)
+                obj.data = struct( ...
+                    'fcn', es, ...
+                    'vs', es );
+            end
         end
 
         function obj = add(obj, var, name, idx, varargin)

lib/+mp/sm_quad_constraint.m
@@ -93,12 +93,14 @@ classdef sm_quad_constraint < mp.set_manager_opt_model
 
             es = struct();  %% empty struct
             obj@mp.set_manager_opt_model(varargin{:});
-            obj.data = struct( ...
-                'Q', es, ...
-                'B', es, ...
-                'l', es, ...
-                'u', es, ...
-                'vs', es );
+            if isempty(obj.data)
+                obj.data = struct( ...
+                    'Q', es, ...
+                    'B', es, ...
+                    'l', es, ...
+                    'u', es, ...
+                    'vs', es );
+            end
         end
 
         function obj = add(obj, var, name, idx, varargin)

lib/+mp/sm_quad_cost.m
@@ -60,11 +60,13 @@ classdef sm_quad_cost < mp.set_manager_opt_model
 
             es = struct();  %% empty struct
             obj@mp.set_manager_opt_model(varargin{:});
-            obj.data = struct( ...
-                'H', es, ...
-                'c', es, ...
-                'k', es, ...
-                'vs', es );
+            if isempty(obj.data)
+                obj.data = struct( ...
+                    'H', es, ...
+                    'c', es, ...
+                    'k', es, ...
+                    'vs', es );
+            end
         end
 
         function new_obj = copy(obj, varargin)

lib/+mp/sm_quad_cost_legacy.m
@@ -66,11 +66,13 @@ classdef sm_quad_cost_legacy < mp.set_manager_opt_model
 
             es = struct();  %% empty struct
             obj@mp.set_manager_opt_model(varargin{:});
-            obj.data = struct( ...
-                'Q', es, ...
-                'c', es, ...
-                'k', es, ...
-                'vs', es );
+            if isempty(obj.data)
+                obj.data = struct( ...
+                    'Q', es, ...
+                    'c', es, ...
+                    'k', es, ...
+                    'vs', es );
+            end
         end
 
         function new_obj = copy(obj, varargin)

lib/+mp/sm_variable.m
@@ -50,11 +50,13 @@ classdef sm_variable < mp.set_manager_opt_model
 
             es = struct();  %% empty struct
             obj@mp.set_manager_opt_model(varargin{:});
-            obj.data = struct( ...
-                'v0', es, ...
-                'vl', es, ...
-                'vu', es, ...
-                'vt', es );
+            if isempty(obj.data)
+                obj.data = struct( ...
+                    'v0', es, ...
+                    'vl', es, ...
+                    'vu', es, ...
+                    'vt', es );
+            end
         end
 
         function obj = add(obj, name, idx, varargin)

commit 1eecd93925068154b96d301ff303ddd41d1c9598

Fix issue caused by ef72097.
This commit caused some MATPOWER code that assumes obj.data is a struct (possibly with no fields) to crash.

lib/+mp/set_manager.m
@@ -149,7 +149,7 @@ classdef set_manager < handle
         order = struct('name', [], 'idx', []);
 
         % *(struct)* additional set-type-specific data for each block
-        data
+        data = struct();
     end     %% properties
 
     methods

lib/+mp/sm_lin_constraint.m
@@ -52,7 +52,7 @@ classdef sm_lin_constraint < mp.set_manager_opt_model
 
             es = struct();  %% empty struct
             obj@mp.set_manager_opt_model(varargin{:});
-            if isempty(obj.data)
+            if isempty(fieldnames(obj.data))
                 obj.data = struct( ...
                     'A', es, ...
                     'l', es, ...

lib/+mp/sm_nln_constraint.m
@@ -47,7 +47,7 @@ classdef sm_nln_constraint < mp.set_manager_opt_model
             %   nln = mp.sm_nln_constraint(label)
 
             obj@mp.set_manager_opt_model(varargin{:});
-            if isempty(obj.data)
+            if isempty(fieldnames(obj.data))
                 obj.data = struct( ...
                     'fcn', [], ...
                     'hess', [], ...

lib/+mp/sm_nln_cost.m
@@ -40,7 +40,7 @@ classdef sm_nln_cost < mp.set_manager_opt_model
 
             es = struct();  %% empty struct
             obj@mp.set_manager_opt_model(varargin{:});
-            if isempty(obj.data)
+            if isempty(fieldnames(obj.data))
                 obj.data = struct( ...
                     'fcn', es, ...
                     'vs', es );

lib/+mp/sm_quad_constraint.m
@@ -93,7 +93,7 @@ classdef sm_quad_constraint < mp.set_manager_opt_model
 
             es = struct();  %% empty struct
             obj@mp.set_manager_opt_model(varargin{:});
-            if isempty(obj.data)
+            if isempty(fieldnames(obj.data))
                 obj.data = struct( ...
                     'Q', es, ...
                     'B', es, ...

lib/+mp/sm_quad_cost.m
@@ -60,7 +60,7 @@ classdef sm_quad_cost < mp.set_manager_opt_model
 
             es = struct();  %% empty struct
             obj@mp.set_manager_opt_model(varargin{:});
-            if isempty(obj.data)
+            if isempty(fieldnames(obj.data))
                 obj.data = struct( ...
                     'H', es, ...
                     'c', es, ...

lib/+mp/sm_quad_cost_legacy.m
@@ -66,7 +66,7 @@ classdef sm_quad_cost_legacy < mp.set_manager_opt_model
 
             es = struct();  %% empty struct
             obj@mp.set_manager_opt_model(varargin{:});
-            if isempty(obj.data)
+            if isempty(fieldnames(obj.data))
                 obj.data = struct( ...
                     'Q', es, ...
                     'c', es, ...

lib/+mp/sm_variable.m
@@ -50,7 +50,7 @@ classdef sm_variable < mp.set_manager_opt_model
 
             es = struct();  %% empty struct
             obj@mp.set_manager_opt_model(varargin{:});
-            if isempty(obj.data)
+            if isempty(fieldnames(obj.data))
                 obj.data = struct( ...
                     'v0', es, ...
                     'vl', es, ...